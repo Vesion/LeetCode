6c53e87e9a578ef62846267c06341289b2d58033
Shine <xuxiang19930924@126.com>
2015-09-19 23:23:06 +0800

    nop

--- algorithm/003-LongestSubstringWithoutRepeatingCharacters.cpp
+++ algorithm/003-LongestSubstringWithoutRepeatingCharacters.cpp
@@ -3,11 +3,18 @@
 #include <string>
 using namespace std;
 
+/*
+ * Using a map store each char's index.
+ * So, we can be easy to know the when duplication and the previous duplicated char's index.
+ * Then we can take out the previous duplicated char, and keep tracking the maxiumn length. 
+ */
 int lengthOfLongestSubstring(string s) {
     int maxLength = 0;
     int lastRepeat = -1;
     unordered_map<char, int> m;
     for (int i=0; i<s.size(); ++i) {
+        // Why while lastRepeat < m[s[i]]?
+        // Beacause the new substring starts from the char which is the closest to i.
         if (m.find(s[i]) != m.end() && lastRepeat < m[s[i]]) {
             lastRepeat = m[s[i]];
         }
