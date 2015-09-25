d01cb0d7fd69c6614baaf1750ad13114d8639419
Shine <xuxiang19930924@126.com>
2015-09-25 16:10:47 +0800

    007 INT_MAX

--- algorithm/007-ReverseInteger.cpp
+++ algorithm/007-ReverseInteger.cpp
@@ -4,16 +4,14 @@
 using namespace std;
 
 int reverse(int x) {
-	const int intmax = 2147483647;
-	const int intmin = -intmax - 1;
 	int y = 0;
 	int n;
 	while (x != 0){
 		n = x % 10;
 		//Checking the over/underflow.
 		//Actually, it should be y>(intmax-n)/10, but n/10 is 0, so omit it.
-		//if (y > (intmax-n) / 10 || y < (intmin-n) / 10){
-		if (y > intmax / 10 || y < intmin / 10){
+		//if (y > (INT_MAX-n) / 10 || y < (INT_MIN-n) / 10){
+		if (y > INT_MAX / 10 || y < INT_MIN / 10){
 			return 0;
 		}
 		y = y * 10 + n;
@@ -23,9 +21,7 @@ int reverse(int x) {
 }
 
 int main() {
-	int n = 2147483648;
-	int m = -123;
-	cout << reverse(m) << endl;
+	cout << reverse(222222) << endl;
 	int a;
 	cin >> a;
 	return 0;
