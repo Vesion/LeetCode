3b4396366627951a7cd757073f697067554bc33a
Shine <xuxiang19930924@126.com>
2015-09-26 10:38:21 +0800

    011

--- algorithm/011-ContainerWithMostWater.cpp
+++ algorithm/011-ContainerWithMostWater.cpp
@@ -17,7 +17,7 @@ int maxArea1(vector<int> &height) {
     return maxarea;
 }
 
-// time limit exceeded
+// ditto, time limit exceeded
 int maxArea2(vector<int> &height) {
     int maxarea = 0;
     for (int w = 1; w <= height.size()-1; ++w) {
@@ -35,12 +35,41 @@ int maxArea2(vector<int> &height) {
     return maxarea;
 }
 
+// solutions above are O(n2), beacause they calculate all area cases
+// we can use `two pointer` scan from two sides to middle, 
+int maxArea(vector<int> &height) {
+    int maxarea = 0;
+    int left = 0, right = height.size()-1;
+    while (left < right) {
+        int area = (right-left) * min(height[left], height[right]);
+        if (area > maxarea)
+            maxarea = area;
+        // because the area is decided by the shorter edge
+        // so we increase the shorter edge is to increase the area
+        //
+        //     height[left] < height[right] ? left++ : right-- ;
+        //
+        // However, this could cause the unnecessary `area` cacluation
+        // We can do some improvement as below:
+        if (height[left] < height[right]) {
+            do {
+                left++;
+            } while (left < right && height[left-1] >= height[left]);
+        } else {
+            do {
+                right--;
+            } while (left < right && height[right+1] >= height[right]);
+        }
+    }
+    return maxarea;
+}
+
 int main() {
     vector<int> p;
     p.push_back(1);
     p.push_back(2);
     p.push_back(3);
     p.push_back(4);
-    cout<<maxArea2(p)<<endl;
+    cout<<maxArea(p)<<endl;
     return 0;
 }
