49607a7050b6be923ee674afd02836cc19e45197
Shine <xuxiang19930924@126.com>
2015-12-08 23:39:12 +0800

    027 implement remove

--- algorithm/027-RemoveElement.cpp
+++ algorithm/027-RemoveElement.cpp
@@ -8,6 +8,19 @@ int removeElement(vector<int>& nums, int val) {
     return distance(nums.begin(), remove(nums.begin(), nums.end(), val));
 }
 
+// implement STL remove manually
+vector<int>::iterator remove(vector<int>::iterator first, vector<int>::iterator last, int val) {
+    vector<int>::iterator result = first;
+    while (first != last) {
+        if (*first != val) {
+            *result = *first;
+            ++result;
+        }
+        ++first;
+    }
+    return result;
+}
+
 int main() {
     vector<int> a{4, 5};
     cout << removeElement(a, 4);
