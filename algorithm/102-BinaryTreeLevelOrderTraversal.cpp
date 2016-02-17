4d767422df10a65b8f0c7f265857637b42881203
Shine <xuxiang19930924@126.com>
2016-02-17 15:05:41 +0800

    102 and 107, BST level order

---
+++ algorithm/102-BinaryTreeLevelOrderTraversal.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+void deleteTree(TreeNode* &root) {
+    if (!root) return;
+    deleteTree(root->left);
+    deleteTree(root->right);
+    delete root;
+    root = NULL;
+}
+
+vector<vector<int>> levelOrder(TreeNode* root) {
+    vector<vector<int>> result;
+    if (!root) return result;
+    queue<TreeNode*> q;
+    q.push(root);
+    while (!q.empty()) {
+        int len = q.size();
+        vector<int> v;
+        while (len--) {
+            auto it = q.front();
+            q.pop();
+            if (it->left) q.push(it->left);
+            if (it->right) q.push(it->right);
+            v.push_back(it->val);
+        }
+        result.push_back(v);
+    }
+    return result;
+}
+
+int main() {
+    TreeNode* root = new TreeNode(2);
+    root->left = new TreeNode(4);
+    root->right = new TreeNode(5);
+    root->right->right = new TreeNode(5);
+    for (auto i : levelOrder(root)) {
+        for (auto j : i)
+            cout << j << " ";
+        cout << endl;
+    }
+    return 0;
+}
