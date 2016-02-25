895da24c4d6b67e9e1803e861a922d4054b511a9
Shine <xuxiang19930924@126.com>
2016-02-25 20:35:16 +0800

    update 003

--- algorithm/003-LongestSubstringWithoutRepeatingCharacters.cpp
+++ algorithm/003-LongestSubstringWithoutRepeatingCharacters.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <unordered_map>
+#include <vector>
 #include <string>
 using namespace std;
 
@@ -9,26 +10,24 @@ using namespace std;
  * So, we can be easy to know the when duplication and the previous duplicated char's index.
  * Then we can take out the previous duplicated char, and keep tracking the maxiumn length. 
  */
+
 int lengthOfLongestSubstring(string s) {
-    int maxLength = 0;
-    int lastRepeat = -1;
-    unordered_map<char, int> m;
-    for (int i=0; i<s.size(); ++i) {
-        // Why while lastRepeat < m[s[i]]?
-        // Beacause the new substring starts from the char which is the closest to i.
-        if (m.find(s[i]) != m.end() && lastRepeat < m[s[i]]) {
-            lastRepeat = m[s[i]];
-        }
-        if (i - lastRepeat > maxLength) {
-            maxLength = i - lastRepeat;
-        }
-        m[s[i]] = i;
+    int n = s.size();
+    if (n <= 1) return n;
+    int result = 0;
+    int start = 0; // store the last repeated character's position
+    vector<int> table(256, -1); // use vector rather than unordered_map, much faster
+    for (int i = 0; i < n; ++i) {
+        if (table[s[i]] >= start) // if this char has occurred and its last occurred position is after start
+            start = table[s[i]] + 1; // refresh start
+        result = max(result, i-start+1); // maitain result with greedy
+        table[s[i]] = i; // store every char's newest position
     }
-    return maxLength;
+    return result;
 }
 
 int main() {
-    string s1 = "abcabcabcabcd";
+    string s1 = "abba";
     cout<<lengthOfLongestSubstring(s1)<<endl;
     return 0;
 }
