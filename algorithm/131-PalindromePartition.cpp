8c9e1c9b54dc8034e24aa934acf7385ad2bfab3b
Shine <xuxiang19930924@126.com>
2016-02-28 21:53:48 +0800

    131 add dp solution

--- algorithm/131-PalindromePartition.cpp
+++ algorithm/131-PalindromePartition.cpp
@@ -3,6 +3,7 @@
 #include <string>
 using namespace std;
 
+// Solution 1 : dfs-backtracking, trivial, O(n^2)
 bool isPalindrome(string& s, int left, int right) {
     while (left < right && s[left] == s[right]) {
         ++left;
@@ -25,7 +26,7 @@ void dfs(vector<vector<string>>& result, vector<string>& path, string &s, int n,
     }
 }
 
-vector<vector<string>> partition(string s) {
+vector<vector<string>> partition_dfs(string s) {
     vector<vector<string>> result;
     if (s.empty()) return result;
     vector<string> path;
@@ -33,8 +34,41 @@ vector<vector<string>> partition(string s) {
     return result;
 }
 
+
+// Solution 2 : dp, O(n^2)
+// dp[i][j]为真表示s[i...j]是回文，如果s[i] == s[j] and dp[i+1][j-1]，则dp[i][j]为真，即s[i...j]是回文，所以计算出dp就知道了所有的回文子串，那么在构造结果的时候就可以直接用dp来判断，而不需要每次用isPalindrome来判断了。
+
+void search(vector<vector<string>>& result, vector<string>& path, string &s, int n, int start, vector<vector<bool>>& dp) {
+    if (start == n) {
+        result.push_back(path);
+        return;
+    }
+    for (int i = start; i < n; ++i) {
+        if (dp[start][i]) {
+            path.push_back(s.substr(start, i-start+1));
+            search(result, path, s, n, i+1, dp);
+            path.pop_back();
+        }
+    }
+}
+
+vector<vector<string>> partition_dp(string s) {
+    int n = s.size();
+    vector<vector<bool>> dp(n, vector<bool>(n, false));
+
+    for (int i = 0; i < n; ++i) dp[i][i] = true; // single character is palindrome
+    for (int i = n-1; i >= 0; --i) // dp[i][j] need dp[i+1][j-1]
+        for (int j = i; j < n; ++j)
+            if (s[i] == s[j] && ((i+1 > j-1) || dp[i+1][j-1])) dp[i][j] = true;
+    
+    vector<vector<string>> result;
+    vector<string> path;
+    search(result, path, s, s.size(), 0, dp);
+    return move(result);
+}
+
 int main() {
-    auto result = partition("aab");
+    auto result = partition_dp("aab");
     for (auto & i : result) {
         for (auto & j : i)
             cout << j << " ";
