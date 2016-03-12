af07e2d262a8ac3760c6e03aa11719571fad6d8d
Shine <xuxiang19930924@126.com>
2016-03-12 20:50:31 +0800

    080 add comments

--- algorithm/080-RemoveDuplicatesFromSortedArrayII.cpp
+++ algorithm/080-RemoveDuplicatesFromSortedArrayII.cpp
@@ -4,7 +4,7 @@
 #include <iterator>
 using namespace std;
 
-// tweak STL unique, use swapping to replace replacing
+// Soluton 1 : tweak std::unique, call it two time to generate two unique subarray
 vector<int>::iterator myUnique(vector<int>::iterator first, vector<int>::iterator last) {
     if (first == last) return last;
     auto result = first;
@@ -16,7 +16,7 @@ vector<int>::iterator myUnique(vector<int>::iterator first, vector<int>::iterato
     return ++result;
 }
 
-int removeDuplicates(vector<int>& nums) {
+int removeDuplicates_two_scans(vector<int>& nums) {
     vector<int>::iterator result = myUnique(nums.begin(), nums.end());
     sort(result, nums.end());
     result = myUnique(result, nums.end());
@@ -24,8 +24,9 @@ int removeDuplicates(vector<int>& nums) {
     return (result - nums.begin());
 }
 
-// another good solution, and can be apply to occur times is 3, 4, ...
-int removeDuplicates_good(vector<int>& nums) {
+
+// Solution 2 : one scan, can be apply to occur times is 3, 4, ...
+int removeDuplicates_one_scan(vector<int>& nums) {
     int n = nums.size();
     if (n <= 2) return n;
     int result = 2;
@@ -40,7 +41,7 @@ int removeDuplicates_good(vector<int>& nums) {
 int main() {
     vector<int> a{1, 1, 1, 1, 2, 3};
     //cout << removeDuplicates(a) << endl;
-    cout << removeDuplicates_good(a) << endl;
+    cout << removeDuplicates_two_scans(a) << endl;
     cout << endl;
 
     for (auto i : a)
