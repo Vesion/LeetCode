388f8fec4549a5800d8229ee034de84863232ed8
Shine <xuxiang19930924@126.com>
2016-03-19 20:06:51 +0800

    284

---
+++ algorithm/284-PeekingIterator.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Iterator {
+    struct Data;
+	Data* data;
+public:
+	Iterator(const vector<int>& nums);
+	Iterator(const Iterator& iter);
+	virtual ~Iterator();
+	// Returns the next element in the iteration.
+	int next();
+	// Returns true if the iteration has more elements.
+	bool hasNext() const;
+};
+
+class PeekingIterator : public Iterator {
+private:
+    bool hasPeeked;
+    int peekedValue;
+public:
+	PeekingIterator(const vector<int>& nums) : Iterator(nums) {
+        hasPeeked = false;
+        peekedValue = 0;
+	}
+
+    // Returns the next element in the iteration without advancing the iterator.
+	int peek() {
+        if (!hasPeeked) {
+            hasPeeked = true;
+            peekedValue = Iterator::next();
+        }
+        return peekedValue;
+	}
+
+	int next() {
+	    if (hasPeeked) {
+            hasPeeked = false;
+            int result = peekedValue;
+            peekedValue = 0;
+            return result;
+        }
+        return Iterator::next();
+	}
+
+	bool hasNext() const {
+	    return Iterator::hasNext() || hasPeeked;
+	}
+};
+
+int main() {
+    return 0;
+}
