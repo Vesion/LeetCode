3c041ddc127d6c0a2d4630eaee416301c7b2c79d
Shine <xuxiang19930924@126.com>
2016-03-27 22:20:50 +0800

    201 fix time complexity

--- algorithm/201-BitwiseANDOfNumbersRange.cpp
+++ algorithm/201-BitwiseANDOfNumbersRange.cpp
@@ -10,7 +10,7 @@ int rangeBitwiseAnd_bf(int m, int n) {
 }
 
 
-// Solution 2 : O(1)
+// Solution 2 : O(lgn)
 // Consider the bits from low to high. if n > m, the lowest bit will be 0, and then we could transfer the problem to sub-problem: rangeBitwiseAnd(m>>1, n>>1).
 int rangeBitwiseAnd(int m, int n) {
     return (n > m) ? rangeBitwiseAnd(m>>1, n>>1)<<1 : m;
