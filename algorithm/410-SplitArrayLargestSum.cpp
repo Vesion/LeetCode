3da11770acb8c05b4d1cec5d3e8ca4aaefcdd89c
Shine <xuxiang19930924@126.com>
2016-10-02 16:36:55 +0800

    410

---
+++ algorithm/410-SplitArrayLargestSum.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    using ll = long long;
+
+    bool canSplit(vector<int>& nums, int m, ll sum) {
+        int c = 1;
+        ll s = 0;
+        for (auto& num : nums) {
+            s += num;
+            if (s > sum) {
+                s = num;
+                ++c;
+            }
+        }
+        return c <= m;
+    }
+
+    int splitArray(vector<int>& nums, int m) {
+        ll left = 0, right = 0;
+        for (auto& num : nums) {
+            left = max(left, (ll)num);
+            right += num;
+        }
+        while (left < right) {
+            if (left == right) return left;
+            ll mid = left + (right-left)/2;
+            if (canSplit(nums, m, mid))
+                right = mid;
+            else
+                left = mid+1;
+        }
+        return left;
+    }
+};
+
+int main() {
+    Solution s;
+    vector<int> nums = {1, 2, 3, 4, 5};
+    cout << s.splitArray(nums, 2);
+    return 0;
+}
