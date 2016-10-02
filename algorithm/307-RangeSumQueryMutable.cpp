0777cc96f9b2c30cf86d1674513f096ba9efcadd
Shine <xuxiang19930924@126.com>
2016-10-02 17:39:12 +0800

    307

--- algorithm/307-RangeSumQueryMutable.cpp
+++ algorithm/307-RangeSumQueryMutable.cpp
@@ -3,54 +3,113 @@
 #include <numeric>
 using namespace std;
 
-// Solution 0 : a little bit better than brute-force, while sumRange is still O(n) :(
-class NumArray_bf {
+
+// Solution 1 : binary indexed tree.
+class NumArray {
 private:
-    vector<int> sum;
-    vector<int> upt;
-    int left;
+    vector<int> nums;
+    vector<int> sums;
+
 public:
-    NumArray_bf(vector<int> &nums) : sum(nums.size()+1, 0), upt(nums.begin(), nums.end()) {
-        left = INT_MAX;
-        partial_sum(nums.begin(), nums.end(), sum.begin()+1);
+    NumArray(vector<int> &nums) {
+        this->nums = nums;
+        int n = nums.size();
+        sums.resize(n+1, 0);
+        for (int i = 0; i < n; ++i)
+            add(i, nums[i]);
+    }
+
+    void add(int i, int val) {
+        i++;
+        while (i < (int)sums.size()) {
+            sums[i] += val;
+            i += (i & -i);
+        }
+    }
+
+    int query(int i) {
+        i++;
+        int sum = 0;
+        while (i > 0) {
+            sum += sums[i];
+            i -= (i & -i);
+        }
+        return sum;
     }
 
     void update(int i, int val) {
-        left = min(left, i);
-        upt[i] = val;
+        add(i, val-nums[i]);
+        nums[i] = val;
     }
 
     int sumRange(int i, int j) {
-        if (j >= left) {
-            for (int k = left+1; k <= j+1; ++k)
-                sum[k] = sum[k-1] + upt[k-1];
-            left = j;
-        }
-        return sum[j+1] - sum[i];
+        return query(j) - query(i-1);
     }
 };
 
 
-// Solution 1 : segment tree. To be continued :)
-class NumArray {
+// Solution 2 : segment tree.
+class NumArray_st {
+private:
+    struct SegmentNode {
+        int start, end;
+        int sum;
+        SegmentNode *left, *right;
+        SegmentNode(int s, int e) : start(s), end(e), sum(0) {}
+    };
+
+    SegmentNode* root = NULL;
+
+    SegmentNode* build(vector<int>& nums, int start, int end) {
+        if (start > end) return NULL;
+        SegmentNode* node = new SegmentNode(start, end);
+        if (start == end) node->sum = nums[start];
+        else {
+            int mid = start + (end-start)/2;
+            node->left = build(nums, start, mid);
+            node->right = build(nums, mid+1, end);
+            node->sum = node->left->sum + node->right->sum;
+        }
+        return node;
+    }
+
+    void update(SegmentNode* root, int i, int val) {
+        if (root->start == root->end) root->sum = val;
+        else {
+            int mid = root->start + (root->end-root->start)/2;
+            if (i <= mid) update(root->left, i, val);
+            else update(root->right, i, val);
+            root->sum = root->left->sum + root->right->sum;
+        }
+    }
+
+    int query(SegmentNode* root, int start, int end) {
+        if (root->start == start && root->end == end)
+            return root->sum;
+        int mid = root->start + (root->end-root->start)/2;
+        if (end <= mid) return query(root->left, start, end);
+        if (start > mid) return query(root->right, start, end);
+        return query(root->left, start, mid) + query(root->right, mid+1, end);
+    }
+
 public:
-    NumArray(vector<int> &nums) {
-        
+    NumArray_st(vector<int> &nums) {
+        root = build(nums, 0, nums.size()-1);        
     }
 
     void update(int i, int val) {
-        
+        update(root, i, val);
     }
 
     int sumRange(int i, int j) {
-        
+        return query(root, i, j);
     }
 };
 
 
 int main() {
     vector<int> nums = {1, 3, 5};
-    NumArray_bf numArray(nums);
+    NumArray_st numArray(nums);
     cout << numArray.sumRange(0, 2) << endl;
     numArray.update(1, 2);
     cout << numArray.sumRange(0, 2) << endl;
