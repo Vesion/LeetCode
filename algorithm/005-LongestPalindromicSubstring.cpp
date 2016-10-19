11a584d9ba573ee658b012292260e68f48bcc71b
Shine <xuxiang19930924@126.com>
2016-10-19 21:36:05 +0800

    update 005

--- algorithm/005-LongestPalindromicSubstring.cpp
+++ algorithm/005-LongestPalindromicSubstring.cpp
@@ -3,68 +3,109 @@
 #include <string>
 using namespace std;
 
-// Solution 1: brute-force traversal, O(n^2)
+
+// Solution 0 : brute-force, check every substring, O(n^3)
+
+
+// Solution 1: extend from each position, O(n^2)
 // traverse the whole string, start finding the palindrome from each char (extend to left and right), get the longest substr
-string findPalindrome(string s, int l, int r) {
-	int n = s.size();
-	while (l >= 0 && r < n && s[l] == s[r]) {
-		--l;
-		++r;
-	}
-	return s.substr(l + 1, r - l - 1);
-}
-
-string longestPalindrome_n2(string s) {
-	if (s.size() <= 1)
-		return s;
-	string longest, str;
-	for (int i = 0; i < s.size() - 1; i++) {
-		str = findPalindrome(s, i, i);
-		if (str.size() > longest.size())
-			longest = str;
-
-		str = findPalindrome(s, i, i + 1);
-		if (str.size() > longest.size())
-			longest = str;
-	}
-	return longest;
-}
-
-
-// Solution 2 : Manacher's Algorithm, O(n)
-// http://www.felix021.com/blog/read.php?2040
-string preProcess(string& s) {
-    string t = "$#";
-    for (int i = 0; i < s.size(); ++i) t += s.substr(i, 1) + "#";
-    return t;
-}
-string longestPalindrome_n(string s) {
-    if (s.size() < 2) return s;
-    string t = preProcess(s);
-    int n = t.size();
-    vector<int> p(n, 0);
-    int mx = 0, id = 0;
-    for (int i = 1; i < n; ++i) {
-        p[i] = mx > i ? min(p[2*id-i], mx-i) : 1;
-        while (t[i+p[i]] == t[i-p[i]]) ++p[i];
-        if (i + p[i] > mx) {
-            mx = i + p[i];
-            id = i;
+class Solution_1 {
+public:
+    string findPalindrome(string s, int l, int r) {
+        int n = s.size();
+        while (l >= 0 && r < n && s[l] == s[r]) {
+            --l;
+            ++r;
         }
+        return s.substr(l + 1, r - l - 1);
     }
-    int maxLen = 0, center = 0;
-    for (int i = 0; i < n; ++i) {
-        if (p[i] > maxLen) {
-            maxLen = p[i];
-            center = i;
+
+    string longestPalindrome(string s) {
+        if (s.size() <= 1)
+            return s;
+        string longest, str;
+        for (int i = 0; i < s.size() - 1; i++) {
+            str = findPalindrome(s, i, i);
+            if (str.size() > longest.size())
+                longest = str;
+
+            str = findPalindrome(s, i, i + 1);
+            if (str.size() > longest.size())
+                longest = str;
         }
+        return longest;
     }
-    return s.substr(center/2 - maxLen/2, maxLen-1);
-}
+};
+
+
+// Solution 2 : dp, O(n^2)
+// dp[i][j] = dp[i+1][j-1] if s[i] == s[j], else = 0;
+class Solution_2 {
+public:
+    string longestPalindrome(string s) {
+        if (s.empty()) return "";
+        int n = s.size();
+        vector<vector<bool>> dp(n, vector<bool>(n, false));
+        int len = 1, start = 0;
+        for (int i = 0; i < n; ++i) {
+            dp[i][i] = true;
+            if (i < n-1 && s[i] == s[i+1]) {
+                dp[i][i+1] = true;
+                start = i;
+                len = 2;
+            }
+        }
+        for (int l = 3; l <= n; ++l) {
+            for (int i = 0; i+l <= n; ++i) {
+                int j = i+l-1;
+                if (dp[i+1][j-1] && s[i] == s[j]) {
+                    dp[i][j] = true;
+                    start = i;
+                    len = max(len, l);
+                }
+            }
+        }
+        return s.substr(start, len);
+    }
+};
+
+
+// Solution 3 : Manacher's Algorithm, O(n)
+// http://www.cnblogs.com/Lyush/p/3221503.html
+class Solution {
+public:
+    string preProcess(string& s) {
+        string t = "$#";
+        for (int i = 0; i < s.size(); ++i) t += s.substr(i, 1) + "#";
+        return t;
+    }
+    string longestPalindrome_n(string s) {
+        if (s.size() < 2) return s;
+        string t = preProcess(s);
+        int n = t.size();
+        vector<int> p(n, 0);
+        int mx = 0, id = 0;
+        for (int i = 1; i < n; ++i) {
+            p[i] = mx > i ? min(p[2*id-i], mx-i) : 1;
+            while (t[i+p[i]] == t[i-p[i]]) ++p[i];
+            if (i + p[i] > mx) {
+                mx = i + p[i];
+                id = i;
+            }
+        }
+        int maxLen = 0, center = 0;
+        for (int i = 0; i < n; ++i) {
+            if (p[i] > maxLen) {
+                maxLen = p[i];
+                center = i;
+            }
+        }
+        return s.substr(center/2 - maxLen/2, maxLen-1);
+    }
+};
 
 int main() {
-    string s = "bb";
-    cout << longestPalindrome_n2(s) << endl;
-	cout << longestPalindrome_n(s) << endl;
-	return 0;
+    Solution_2 s2;
+    cout << s2.longestPalindrome("aaa") << endl;
+    return 0;
 }
