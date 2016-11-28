b795f4fb8c528da035a36c207fec4c54b56571ec
Shine <xuxiang19930924@126.com>
2016-11-28 15:05:14 +0800

    156 reverse binary tree, recursive

---
+++ algorithm/156-BinaryTreeUpsideDown.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left, *right;
+    TreeNode(int val) : val(val), left(NULL), right(NULL) {}
+};
+
+// Similar to 206-ReverseLinkedList, recursively
+class Solution {
+public:
+    TreeNode* upsideDownBinaryTree(TreeNode* root) {
+        if (!root || (!root->left && !root->right)) return root;
+        TreeNode* newroot = upsideDownBinaryTree(root->left);
+        root->left->right = root;
+        root->left->left = root->right;
+        root->left = root->right = NULL;
+        return newroot;
+    }   
+};
+
+int main() {
+    Solution s;
+    return 0;
+}
